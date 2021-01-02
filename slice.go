b7e3a66866c16804dd7c3f4562467bfda78a65f4
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-01-02 20:03:37 +0530

    Update slice.go

--- slice.go
+++ slice.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	array := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(slice)
+	colors := make([]string, 3)
+	colors[0] = "red"
+	colors[1] = "green"
+	fmt.Println(colors)
+	// array := [5]int{1, 2, 3, 4, 5}
+	// res := array[2:4]
+	// fmt.Println(res)
 }
