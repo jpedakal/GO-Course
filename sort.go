5d1c3cdced643dc94ce65892ec2f1456c0692dff
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-01-04 20:09:07 +0530

    Create sort.go

---
+++ sort.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	// strings sort
+	strs := []string{"a", "b", "f", "c", "z", "q"}
+	sort.Strings(strs)
+
+	// ints sort
+	digits := []int{1, 4, 6, 7, 78, 34, 56, 76,12,22}
+	sort.Ints(digits)
+	fmt.Println(digits)
+}
