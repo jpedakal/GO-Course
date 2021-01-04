5d10de6af3fb4b98e4f55233653833975d4053bf
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-01-04 21:01:57 +0530

    Update hashes.go

--- hashes.go
+++ hashes.go
@@ -10,9 +10,9 @@ func main() {
 	s := "Welcome To DXC"
 	h := sha1.New()
 	h.Write([]byte(s))
-	bs := h.Sum(nil)
-	fmt.Println(s)
 
-	fmt.Printf("%x\n", bs)
+	bs := h.Sum(nil)
+		
+   fmt.Printf("%x\n", bs)
 
 }
