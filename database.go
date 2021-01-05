4a58b11d24f3d4599722b76fdc9456106bbd0394
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-01-05 20:13:37 +0530

    Update database.go

--- database.go
+++ database.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -13,4 +17,16 @@ type Trainer struct {
 func main() {
 	URI := "mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb"
 	clientOptions := options.Client().ApplyURI(URI)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
 }
