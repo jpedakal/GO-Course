beac10dcd3cd048772e25186bd14369e2461850e
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2021-01-05 11:50:56 +0530

    Update strFunction.go

--- strFunction.go
+++ strFunction.go
@@ -8,6 +8,12 @@ import (
 var p = fmt.Println
 
 func main() {
+	str := "test"
 	p("contains:",s.Contains("test","es"))
-	p("count ", s.Count("test","s"))
+	p("count:", s.Count(str,"s"))
+	p("HasPrefix:", s.HasPrefix(str,"te"))
+	p("HasSuffix:", s.HasSuffix(str,"st"))
+	p("Index:", s.Index(str,"e"))
+	p("Join:", s.Join([]string{"a","b"},"-"))
+	p("Repeat", s.Repeat("a",5))
 }
